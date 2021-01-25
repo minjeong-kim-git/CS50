b1153e0dc190adb3562fef75b6deeda8166666dc
Minjeong Kim <70756727+minjeong-kim-git@users.noreply.github.com>
2021-01-25 20:50:52 +0900

    Add files via upload

---
+++ Week3/Array/Array.cpp
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+# include <cs50.h>
+# include <stdio.h>
+
+int main(void)
+{
+	//Scores
+	int scores[3];
+	scores[0] = 72;
+	scores[1] = 73;
+	scores[2] = 33;
+
+	//Print Averages
+	printf("Average: %i\n", scores[0] + scores[1] + scores[2] / 3)
+}
