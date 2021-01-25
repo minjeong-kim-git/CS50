29e8bb42c2a553b876c89dc8027a287bd812bdc4
Minjeong Kim <70756727+minjeong-kim-git@users.noreply.github.com>
2021-01-25 21:20:37 +0900

    Add files via upload

---
+++ Week3/String/String.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	string names[4];
+	names[0] = "Emma";
+	names[1] = "Rodrigo";
+	names[2] = "Josh";
+	names[3] = "David";
+
+	printf("%s\n", names[0]);
+	print("%c%c%c%c\n", names[0][0], names[0][1], names[0][2], names[0][3]);
+}
